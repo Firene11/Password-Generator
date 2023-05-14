d59ae0c984ff236ebbe9ba64ed81837a2e94217d
Fih <124883284+Firene11@users.noreply.github.com>
2023-05-14 17:07:47 -0300

    turned letter variables into an array

--- Develop/script.js
+++ Develop/script.js
@@ -49,19 +49,16 @@ while (!confirmLowerCase && !confirmUpperCase && !confirmNumbers && !confirmSpec
   confirmSpecialChar = confirm("Do you want to include special characters?");
 }
 
-var lowerCase = ["abcdefghijklmnopqrstuvwxyz", true]
-var allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789@#$";
-var passwordMin = 8;
-var passwordMax = 120;
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers = 1, 2, 3, 4, 5, 6, 7, 8, 9, 0;
+var specialChar = ["!", "@", "#", "$", "%", "&", "*", "-", "_"];
 var password = " ";
 
-  console.log("Does this button work?")
   
   return password
 }
 
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
 
 //WHEN all prompts are answered
 //THEN a password is generated that matches the selected criteria

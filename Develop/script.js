482f3649e1e54fe21d14e7a086c4ea34ead4154f
Fih <124883284+Firene11@users.noreply.github.com>
2023-05-14 12:15:42 -0300

    added character length prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,30 @@
 // Assignment Code
 
 function generatePassword() {
+  var passwordLength = prompt("How many characters would you like your password to be?")
+
   console.log("Does this button work?")
+  
+
+//WHEN I click the button to generate a password
+//THEN I am presented with a series of prompts for password criteria
+
+
+//WHEN prompted for the length of the password
+//THEN I choose a length of at least 8 characters and no more than 128 characters
+
+//WHEN asked for character types to include in the password
+//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+//WHEN I answer each prompt
+//THEN my input should be validated and at least one character type should be selected
+
+//WHEN all prompts are answered
+//THEN a password is generated that matches the selected criteria
+
+//WHEN the password is generated
+//THEN the password is either displayed in an alert or written to the page
+
   return "God Help me"
 }
 

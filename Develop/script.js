f4708a9dd420fd0decbb42a3cc74148d556db7bf
Fih <124883284+Firene11@users.noreply.github.com>
2023-05-14 21:17:33 -0300

    Added loop for call on password but is only displaying last character

--- Develop/script.js
+++ Develop/script.js
@@ -3,14 +3,15 @@
 function generatePassword() {
 
 // Prompt user to choose a number
-  var pwLength = Number(prompt("How many characters would you like your password to be?"));
+  var pwLength = prompt("How many characters would you like your password to be?");
 
 // Creates a loop for number length requirement
-while (pwLength < 8 || pwLength > 128 || isNaN(pwLength)) { 
-  var pwLength = Number(prompt("Password length must be at least 8 - 128 characters. How many characters would you like your password to be?"));
+while (pwLength < 8 || pwLength > 128) { 
+  var pwLength = prompt("Password length must be at least 8 - 128 characters. How many characters would you like your password to be?");
 }
+
 // Confirm password length
-alert("Your password will have " + (pwLength) + "characters. ");
+alert("Your password will have " + (pwLength) + " " + "characters. ");
 
 // Output of Confirm buttons on rest of requirement questions
 var confirmLowerCase = confirm("Do you want to include lowercase letters? ");
@@ -53,8 +54,7 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var specialChar = ["!", "@", "#", "$", "%", "&", "*", "-", "_"];
-
-var ifChosen = " ";
+var ifChosen = [ ];
       
 if (confirmLowerCase) {
   ifChosen = ifChosen.concat(lowerCase)
@@ -73,15 +73,14 @@ if (confirmSpecialChar) {
 }
 console.log(ifChosen);
 
-var crazyPassword = [];
+var genPassword = [];
+
 
 for (var i = 0; i <= pwLength; i++) {
-  var crazyPassword = Math.floor(Math.random() * pwLength.length);
-  console.log(crazyPassword);
+  genPassword = ifChosen[Math.floor(Math.random() * ifChosen.length)];
+  console.log(genPassword);
 }
-
-
-return crazyPassword
+return genPassword;
 }
 
 var generateBtn = document.querySelector("#generate");
